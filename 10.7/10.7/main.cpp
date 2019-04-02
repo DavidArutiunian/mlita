4ffe022440495f40e82e571144aebc28af60f49a
David Arutiunian <arutunjan666@mail.com>
2019-04-02 09:26:01 +0300

    New bunch of fixes to 10.7 (￢_￢;)

--- 10.7/10.7/main.cpp
+++ 10.7/10.7/main.cpp
@@ -9,11 +9,11 @@ Visual Studio 2017
 
 #include "pch.h"
 
-#define DEBUG true
+#define DEBUG false
 
 using t_number = long long;
 using t_prices = std::vector<t_number>;
-using t_disc = std::vector <t_number>;
+using t_disc = std::vector<t_number>;
 using t_discs = std::vector<t_disc>;
 
 const std::size_t PRICE_LINE_COUNT = 3;
@@ -30,6 +30,8 @@ inline void set_discs(std::vector<t_number> const& actual_prices, t_prices const
 
 inline t_discs restore_initial_sort(t_prices const& initial_prices, t_discs const& discs);
 
+inline t_number get_rest_balance(t_number initial, t_prices const& prices, std::vector<t_number> const&& discs);
+
 enum position
 {
     first = 0,
@@ -39,8 +41,8 @@ enum position
 
 enum disc
 {
-	position = 0,
-	price
+    position = 0,
+    price
 };
 
 int main()
@@ -55,7 +57,6 @@ int main()
         std::sort(prices.begin(), prices.end());
         process(balance, prices, discs);
         write(balance, restore_initial_sort(initial_prices, discs));
-        
     }
     catch (const std::exception& ex)
     {
@@ -65,59 +66,61 @@ int main()
 
 void process(t_number& balance, t_prices& prices, t_discs& discs)
 {
-	const auto initial_balance = balance;
-	auto last_balance = balance;
-	t_number last_total = 0;
+    const auto initial_balance = balance;
+    auto last_balance = balance;
+    t_number last_total = 0;
     for (std::size_t i = 0; i < prices.size() - 1; ++i)
     {
-		const auto total = initial_balance / prices[i];
+        const auto total = initial_balance / prices[i];
         if (total < last_total)
         {
-			continue;
+            continue;
         }
-		last_total = total;
-		const auto div = (prices[i + 1] - prices[i]);
+        last_total = total;
+        const auto div = (prices[i + 1] - prices[i]);
         if (div == 0)
         {
-			continue;
+            continue;
         }
-		const auto second = (initial_balance - total * prices[i]) / div;
-		const auto first = total - second;
+        const auto second = (initial_balance - total * prices[i]) / div;
+        const auto first = total - second;
         if (total == 0 && second == 0 && first == 0)
         {
-			continue;
+            continue;
         }
-		const auto third = total - (first + second);
-		last_balance = initial_balance - (first * prices[i] + second * prices[i + 1]);
-		if (last_balance < balance)
-		{
-			balance = last_balance;
-			set_discs({ first, second, third }, prices, discs);
-		}
-		for (auto j = 0; j < total - first; ++j)
-		{
-			const auto next_first = first + j - 1;
-			const auto next_second = second - j;
-			const auto next_third = third + j - (j - 1);
-			last_balance = initial_balance - next_first * prices[position::first] - next_second * prices[position::second] - next_third * prices[position::third];
-			if (last_balance < balance && last_balance >= 0)
-			{
-				balance = last_balance;
-				set_discs({ next_first , next_second, next_third }, prices, discs);
-			}
-		}
-		for (auto j = 0; j < total - first; ++j)
-		{
-			const auto next_first = first + j - 1;
-			const auto next_second = third + j - (j - 1);
-			const auto next_third = second - j;
-			last_balance = initial_balance - next_first * prices[position::first] - next_second * prices[position::second] - next_third * prices[position::third];
-			if (last_balance < balance && last_balance >= 0)
-			{
-				balance = last_balance;
-				set_discs({ next_first , next_second, next_third }, prices, discs);
-			}
-		}
+        const auto third = total - (first + second);
+        last_balance = get_rest_balance(initial_balance, prices, {first, second, third});
+        if (last_balance < balance)
+        {
+            balance = last_balance;
+            set_discs({first, second, third}, prices, discs);
+        }
+        for (auto j = 0; j < total - first; ++j)
+        {
+            const auto next_first = first + j;
+            for (auto k = 0; k < total - next_first; ++k)
+            {
+                const auto next_second = total - next_first - k;
+                const auto next_third = k;
+                last_balance = get_rest_balance(initial_balance, prices, {next_first, next_second, next_third});
+                if (last_balance <= balance && last_balance >= 0)
+                {
+                    balance = last_balance;
+                    set_discs({next_first, next_second, next_third}, prices, discs);
+                }
+            }
+            for (auto k = 0; k < total - next_first; ++k)
+            {
+                const auto next_second = k;
+                const auto next_third = total - next_first - k;
+                last_balance = get_rest_balance(initial_balance, prices, {next_first, next_second, next_third});
+                if (last_balance <= balance && last_balance >= 0)
+                {
+                    balance = last_balance;
+                    set_discs({next_first, next_second, next_third}, prices, discs);
+                }
+            }
+        }
     }
 }
 
@@ -172,10 +175,10 @@ t_number get_sum_of_discs(t_discs const& discs)
 
 void set_discs(std::vector<t_number> const& actual_prices, t_prices const& prices, t_discs& discs)
 {
-	discs.clear();
-	discs.push_back({ prices[first], actual_prices[first] });
-	discs.push_back({ prices[second], actual_prices[second] });
-	discs.push_back({ prices[third], actual_prices[third] });
+    discs.clear();
+    discs.push_back({prices[first], actual_prices[first]});
+    discs.push_back({prices[second], actual_prices[second]});
+    discs.push_back({prices[third], actual_prices[third]});
 }
 
 t_discs restore_initial_sort(t_prices const& initial_prices, t_discs const& discs)
@@ -183,13 +186,18 @@ t_discs restore_initial_sort(t_prices const& initial_prices, t_discs const& disc
     auto discs_copy(discs);
     for (std::size_t i = 0; i < discs_copy.size(); ++i)
     {
-		for (auto j = i; j < discs_copy.size() - 1; ++j)
-		{
-			if (initial_prices[i] != discs_copy[j][position])
-			{
-				std::swap(discs_copy[j], discs_copy[j + 1]);
-			}
-		}
+        for (auto j = i; j < discs_copy.size() - 1; ++j)
+        {
+            if (initial_prices[i] != discs_copy[j][position])
+            {
+                std::swap(discs_copy[j], discs_copy[j + 1]);
+            }
+        }
     }
     return discs_copy;
+}
+
+t_number get_rest_balance(t_number initial, t_prices const& prices, std::vector<t_number> const&& discs)
+{
+    return initial - discs[first] * prices[first] - discs[second] * prices[second] - discs[third] * prices[third];
 }
